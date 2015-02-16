e71aa13c59eb82ad6ee14fae56e7aed22d76c2f2
Kevin Gary <kgary@asu.edu>
2015-02-16 09:06:53 -0700

    Some starting js

---
+++ basicjs/objects1.js
@@ -0,0 +1,11 @@
+function Circle(radius) {
+  this.radius = radius;
+  
+  this.getArea = 
+    function() {
+      return(Math.PI * this. radius * this.radius);
+    };
+}
+
+var c = new Circle(10);
+console.log("Area: " + c.getArea());         
